a18f5c99531870f82226b3e3b4050054013d67ed
StanislavRyzhkov <30403280+StanislavRyzhkov@users.noreply.github.com>
2019-12-16 21:45:00 +0700

    Update LinkedRecursiveListTest.kt

--- src/test/kotlin/company/ryzhkov/LinkedRecursiveListTest.kt
+++ src/test/kotlin/company/ryzhkov/LinkedRecursiveListTest.kt
@@ -4,24 +4,27 @@ import company.ryzhkov.list.Empty
 import company.ryzhkov.list.InnerList
 import company.ryzhkov.list.LinkedRecursiveList
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 class LinkedRecursiveListTest {
+    var list: LinkedRecursiveList<Int> = Empty
 
-
-    @Test fun getLastElementTest() {
-        var list: LinkedRecursiveList<Int> = Empty
+    @Before fun init() {
         for (i in 0 until 10) {
             list = InnerList(i, list)
         }
+    }
+
+    @Test fun getLastElementTest() {
         Assert.assertEquals(0, list[list.size - 1])
     }
 
+    @Test fun getElement0Test() {
+        Assert.assertEquals(9, list[0])
+    }
+
     @Test fun sizeTest() {
-        var list: LinkedRecursiveList<Int> = Empty
-        for (i in 0 until 10) {
-            list = InnerList(i, list)
-        }
         Assert.assertEquals(10, list.size)
     }
 }
